2fd06fb64739030384e5ceafb798a8daa33458f5
danudeep90 <danudeep90@gmail.com>
2021-07-19 11:21:42 +0530

    shell script for env setup

---
+++ env_setup.sh
@@ -0,0 +1,14 @@
+# Creating venv
+python3 -m venv venv
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Change directory
+cd yolov5
+
+# Install requirements
+pip install -r requirements.txt
+
+# Install google cloud related
+pip install google-cloud-storage
